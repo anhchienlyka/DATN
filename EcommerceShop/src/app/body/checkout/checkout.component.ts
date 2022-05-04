d3a39fb87aca375094952127c0a552621682805e
chien <chien@DESKTOP-5EHPFDI>
2022-05-05 01:07:04 +0700

    bnmbn

--- EcommerceShop/src/app/body/checkout/checkout.component.ts
+++ EcommerceShop/src/app/body/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ProductOrder } from 'src/app/model/cart.model';
 import { OrderDetail } from 'src/app/model/orderDetail.model';
 import { User } from 'src/app/model/user.model';
+import { AccountService } from 'src/app/Services/account.service';
 import { CartService } from 'src/app/Services/cart.service';
 import { CheckoutService } from 'src/app/Services/checkout.service';
 
@@ -15,19 +16,21 @@ export class CheckoutComponent implements OnInit {
 
   currentUser: User;
   productsInCart: ProductOrder[];
-  quantity?: any; 
-  total_price?:number;
+  quantity?: any;
+  total_price?: number;
   totalCost?: number;
   orderDetailsInOrder: OrderDetail[] = [];
-  constructor(private cartService: CartService,private checkoutService:CheckoutService) {}
+  constructor(private cartService: CartService, private checkoutService: CheckoutService,private accountService : AccountService) { }
   //constructor(private authenticationService: AuthenticationService,private cartService: CartService, private orderService:OrderService) {}
 
-  
+
 
   ngOnInit(): void {
+    this.getCurrentUser();
+    this.checkoutAccount();
   }
 
-  onSubmit(){
+  onSubmit() {
 
   }
 
@@ -38,14 +41,37 @@ export class CheckoutComponent implements OnInit {
     phone: new FormControl(''),
     email: new FormControl(''),
   });
-
-  checkoutAccount() {
+  checkoutAccount
+    () {
     this.currentUserForm.controls['fullName'].setValue(this.currentUser.fullName);
     this.currentUserForm.controls['userName'].setValue(this.currentUser.userName);
     this.currentUserForm.controls['address'].setValue(this.currentUser.address);
     this.currentUserForm.controls['phone'].setValue(this.currentUser.phone);
     this.currentUserForm.controls['email'].setValue(this.currentUser.email);
-    
+
+  }
+
+
+  getCurrentUser() {
+    this.currentUser = this.accountService.getCurrentUser();
+  }
+  transfer(){
+    for(let i = 0 ; i< this.productsInCart.length; i++ ){
+      let object1:OrderDetail = {
+        orderId: 0,
+        productId: 0,
+        price: 0,
+        total_Price: 0,
+        quantity:0
+      }
+      object1.productId = this.productsInCart[i].productId;
+      object1.price = this.productsInCart[i].price;
+    //  object1.total_Price =this.totalPrice;
+      object1.quantity= this.productsInCart[i].quantity;
+      this.orderDetailsInOrder.push(object1)
+    }
+
+    // console.log(this.orderDetailsInOrder)
   }
 
 
@@ -60,14 +86,10 @@ export class CheckoutComponent implements OnInit {
 
 
 
-
-
-
-  
-   myFunction() {
-    var element = <HTMLInputElement> document.getElementById("shipto");
+  myFunction() {
+    var element = <HTMLInputElement>document.getElementById("shipto");
     var text = document.getElementById("mytext");
-    if (element.checked == true){
+    if (element.checked == true) {
       text.style.display = "block";
     } else {
       text.style.display = "none";
@@ -76,20 +98,20 @@ export class CheckoutComponent implements OnInit {
 
 
   myFunction2() {
-    var element = <HTMLInputElement> document.getElementById("payment-2");
+    var element = <HTMLInputElement>document.getElementById("payment-2");
     var text = document.getElementById("payment-2-show");
- 
-    if (element.checked == true){
+
+    if (element.checked == true) {
       text.style.display = "block";
     }
   }
   myFunction3() {
-    var element = <HTMLInputElement> document.getElementById("payment-1");
+    var element = <HTMLInputElement>document.getElementById("payment-1");
     var text = document.getElementById("payment-2-show");
- 
-    if (element.checked == true){
+
+    if (element.checked == true) {
       text.style.display = "none";
-   text.style.display = "none";
+      text.style.display = "none";
     }
   }
 
